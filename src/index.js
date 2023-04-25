24ea23ab6e28262e6023196f00e3e466dc8bcc3f
Natalie Mezher <nataliemezher96@hotmail.com>
2023-04-25 10:49:57 +0200

    file structure

--- src/index.js
+++ src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//Layout
+import './style/layout/generic.css';
+import './style/layout/header.css';
+import './style/layout/footer.css';
+
+//Section
+import './style/section/hero.css';
+import './style/section/projects.css';
+
+//Components
+import './style/components/button.css';
+
+//Parts
+import './style/parts/project_card.css';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
