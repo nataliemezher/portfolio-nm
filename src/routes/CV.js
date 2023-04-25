aceadcf4001feb952ba8ddcaf779596112859d5f
Natalie Mezher <nataliemezher96@hotmail.com>
2023-04-25 16:11:49 +0200

    cv render route

---
+++ src/routes/CV.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/layout/Navbar";
+const CV = () => {
+    return (
+        <div className="content">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default CV
