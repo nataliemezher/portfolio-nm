593f777a180a56d3b4664b3c78d061c2845905b3
Natalie Mezher <nataliemezher96@hotmail.com>
2023-04-25 15:58:12 +0200

    adds routes to pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import Content from './components/layout/Content';
+
+/* import Footer from './components/layout/Footer';
+import Content from './components/layout/Content'; */
+
+import Home from './routes/Home';
+import Projects from './routes/Projects';
+import CV from './routes/CV';
+import Contact from './routes/Contact';
+
+import {Route, Routes} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <div class="content">
-        <Header />
-        <Content />
-        <Footer />
-      </div>
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/cv" element={<CV/>}/>
+      <Route path="/projects" element={<Projects/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+    </Routes>
+    </>
   );
 }
 
