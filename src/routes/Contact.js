d7f58f0f192767d1b2e1794d5d11a8ec09b6f34b
Natalie Mezher <nataliemezher96@hotmail.com>
2023-04-25 15:54:44 +0200

    adds pages in routes folder

---
+++ src/routes/Contact.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/layout/Navbar";
+const Contact = () => {
+    return (
+        <div className="content">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Contact
---
+++ src/routes/Home.js
@@ -0,0 +1,10 @@
+import Navbar from "../components/layout/Navbar";
+const Home = () => {
+    return (
+        <div className="content">
+            <Navbar/>
+        </div>
+    )
+}
+
+export default Home
